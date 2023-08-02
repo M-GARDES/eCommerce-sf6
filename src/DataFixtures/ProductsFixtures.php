16dcbd6d3a39a955e0cdd8874ef17bb64edd5b7e
magali gardes <gardesmagali@gmail.com>
2023-08-02 05:01:48 +0200

    FakeProducts

--- src/DataFixtures/ProductsFixtures.php
+++ src/DataFixtures/ProductsFixtures.php
@@ -4,14 +4,33 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Monolog\DateTimeImmutable;
+use Faker;
+
+use App\Entity\Products;
 
 class ProductsFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
 
+        $faker = Faker\Factory::create('fr_FR');
+
+        for ($prod = 1; $prod <= 10; $prod++) {
+            $product = new Products();
+            $product->setName($faker->text(15));
+            $product->setDescription($faker->text());
+            $product->setPrice($faker->numberBetween(900, 150000));
+            $product->setStock($faker->numberBetween(0, 50));
+            $product->setCreatedAt(new DateTimeImmutable('Y-m-d H:i:s'));
+
+            $category = $this->getReference('cat-' . rand(1, 8));
+            $product->setCategories($category);
+
+            $manager->persist($product);
+            $this->addReference('prod-'.$prod, $product);
+          
+        }
         $manager->flush();
     }
 }
