2d7d3d6c5d058280315f4f0a29bbb1ed61ce1e99
magali gardes <gardesmagali@gmail.com>
2023-08-03 05:56:20 +0200

    cngmt

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,8 +11,6 @@ class MainController extends AbstractController
     #[Route('/', name: 'app_main')]
     public function index(): Response
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
-        ]);
+        return $this->render('main/index.html.twig');
     }
 }
