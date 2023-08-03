ae621a0603be85650cd5f5831ebcaf18ad30c0b8
magali gardes <gardesmagali@gmail.com>
2023-08-03 22:49:39 +0200

    scrip_caté_list

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -6,17 +6,14 @@ use App\Entity\Categories;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 #[Route('/categories', name: 'categories_')]
 class CategoriesController extends AbstractController
 {
     
     #[Route('/{id}', name: 'list')]
-    #[ParamConverter('categories', class: 'App\Entity\Categories')]
     public function list(Categories $categories): Response
     {
-        $produitDeLaliste = $category->getProducts();
         return $this->render('categories/list.html.twig', compact('category'));
     }
 }
