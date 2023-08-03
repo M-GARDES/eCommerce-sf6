135ed939293046d083bc1086593f177c1c59780e
magali gardes <gardesmagali@gmail.com>
2023-08-03 07:19:58 +0200

    Maj

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Controller;
-
+use App\Repository\CategoriesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +9,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(): Response
+    public function index(CategoriesRepository $categoriesRepository): Response
     {
-        return $this->render('main/index.html.twig');
-    }
-}
+        return $this->render('main/index.html.twig', [
+            'categories' => $categoriesRepository->findBy([],
+            ['categoryOrder' => 'asc'])
+        ]);
+    }    
+}        
+   
