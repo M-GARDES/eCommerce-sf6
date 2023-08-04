55022cb62dd48b5d9eab8d003bda36d8680c3c1d
magali gardes <gardesmagali@gmail.com>
2023-08-04 17:45:03 +0200

    route

--- src/Controller/Admin/ProductsController.php
+++ src/Controller/Admin/ProductsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Products;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,6 +12,21 @@ class ProductsController extends AbstractController
 {
     #[Route('/', name:'index')]
     public function index(): Response {
+        return $this->render('admin/products/index.html.twig');
+    }
+
+    #[Route('/ajout', name:'add')]
+    public function add(): Response {
+        return $this->render('admin/products/index.html.twig');
+    }
+
+    #[Route('/edition/{id}', name:'editx')]
+    public function edit(Products $products): Response {
+        return $this->render('admin/products/index.html.twig');
+    }
+
+    #[Route('/supression/{id}', name:'delete')]
+    public function delete(Products $products): Response {
         return $this->render('admin/products/index.html.twig');
     }
 } 
