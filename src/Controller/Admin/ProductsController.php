f43c1ca557165b1f994df41eb4d503c680c42233
magali gardes <gardesmagali@gmail.com>
2023-08-05 22:46:31 +0200

    crea_flash+

--- src/Controller/Admin/ProductsController.php
+++ src/Controller/Admin/ProductsController.php
@@ -4,9 +4,12 @@ namespace App\Controller\Admin;
 
 use App\Entity\Products;
 use App\Form\ProductsFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/admin/produits', name:'admin_products_')]
 class ProductsController extends AbstractController
@@ -17,7 +20,8 @@ class ProductsController extends AbstractController
     }
 
     #[Route('/ajout', name:'add')]
-    public function add(): Response {
+    public function add(Request $request, EntityManagerInterface $em, SluggerInterface $slugger): Response {
+
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
 
         //creation'nouveau produit'
@@ -25,6 +29,31 @@ class ProductsController extends AbstractController
 
         //crea form
         $productForm = $this->createForm(ProductsFormType::class,$product);
+
+        //traitement de la requete du formulaire
+        $productForm->handleRequest($request);
+
+        //dd($productForm); //pour le dump
+
+        //verification si le formulaire est soumit et valide
+        if($productForm->isSubmitted() && $productForm->isValid())
+        //generation du slugg
+            //$slug = $slugger->slug($product->getName()); 
+            //dd($slug);
+            //$product->setSlug($slug);
+            //arrondir le prix 
+            $prix = $product->getPrice() * 100;
+            $product->setPrice($prix);
+
+            //dd($prix);
+
+            $em->persist($product);//stokker les infos et executer dns la bdd
+            $em->flush();
+
+            $this->addFlash()
+
+
+
 
         return $this->render('admin/products/add.html.twig',[
           'productForm' => $productForm->createView()
