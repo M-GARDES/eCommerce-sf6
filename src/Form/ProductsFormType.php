babb26ce88bf4f5a3f5e850612e073230f54ebb8
magali gardes <gardesmagali@gmail.com>
2023-08-05 21:20:45 +0200

    formCategrs

--- src/Form/ProductsFormType.php
+++ src/Form/ProductsFormType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Categories;
 use App\Entity\Products;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,11 +14,20 @@ class ProductsFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', options:[
+                'label' => 'Nom'
+            ])
             ->add('description')
-            ->add('price')
-            ->add('stock')
-            //->add('categories')
+            ->add('price', options:[
+                'label' => 'Prix'
+            ])
+            ->add('stock', options:[
+                'label' => 'Unité en stock'
+            ])
+            ->add('categories', EntityType::class, [
+                'class' => Categories::class,
+                'choice_label' => 'name'
+            ])
         ;
     }
 
