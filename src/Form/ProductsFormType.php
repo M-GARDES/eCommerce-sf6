070f77e06a8b007bf2ec88e212637b1027f7d59a
magali gardes <gardesmagali@gmail.com>
2023-08-05 22:05:46 +0200

    ascCat

--- src/Form/ProductsFormType.php
+++ src/Form/ProductsFormType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Categories;
 use App\Entity\Products;
+use App\Repository\CategoriesRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -26,7 +27,15 @@ class ProductsFormType extends AbstractType
             ])
             ->add('categories', EntityType::class, [
                 'class' => Categories::class,
-                'choice_label' => 'name'
+                'choice_label' => 'name' ,
+                'label' => 'catégorie',
+                'group_by' => 'parent.name',
+                'query_builder' => function(CategoriesRepository $cr)
+                {
+                    return $cr->createQueryBuilder('c')
+                    ->where('c.parent IS NOT NULL')
+                    ->orderBy('c.name', 'ASC');
+                }
             ])
         ;
     }
