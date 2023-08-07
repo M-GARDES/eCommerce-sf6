ce3a48386e032bc1510270fabfcfb7dfa1a1d1ec
magali gardes <gardesmagali@gmail.com>
2023-08-07 23:05:32 +0200

    modiftheme5

--- src/Controller/Admin/ProductsController.php
+++ src/Controller/Admin/ProductsController.php
@@ -9,65 +9,64 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 
 #[Route('/admin/produits', name:'admin_products_')]
 class ProductsController extends AbstractController
 {
     #[Route('/', name:'index')]
-    public function index(): Response {
-
-    {
-        $produits = $productsRepository->findAll();
-    }    
+    public function index(): Response 
+    {     
         return $this->render('admin/products/index.html.twig');
     }
 
     #[Route('/ajout', name:'add')]
-    public function add(Request $request, EntityManagerInterface $em): Response {
+    public function add(): Response 
+    {
 
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        return $this->render('admin/product/index.html.twig');
 
         //creation'nouveau produit'
         $product = new Products();
-
         //crea form
         $productForm = $this->createForm(ProductsFormType::class,$product);
 
         //traitement de la requete du formulaire
-        $productForm->handleRequest($request);
+        //$productForm->handleRequest($request);
 
         //dd($productForm); //pour le dump
 
         //verification si le formulaire est soumit et valide
-        if($productForm->isSubmitted() && $productForm->isValid())
-        {
-            $images = $productForm->get('images')->getData();
-            $folder = 'products';
+        //if($productForm->isSubmitted() && $productForm->isValid())
+       // {
+          //  $images = $productForm->get('images')->getData();
+          //  $folder = 'products';
             //arrondir le prix 
-            $prix = $product->getPrice() * 100;
-            $product->setPrice($prix);
+          //  $prix = $product->getPrice() * 100;
+          //  $product->setPrice($prix);
 
             //dd($prix);
 
-            $em->persist($product);//stokker les infos et executer dns la bdd
-            $em->flush();//et executer dns la bdd
+           // $em->persist($product);//stokker les infos et executer dns la bdd
+           // $em->flush();//et executer dns la bdd
 
             
-            return $this->redirectToRoute('admin_products_index');//redirige
-        }
+        //    return $this->redirectToRoute('admin_products_index');//redirige
+        //}
 
-            return $this->render('admin/products/add.html.twig',[
-            'productForm' => $productForm->createView()
-        ]);
+        //    return $this->render('admin/products/add.html.twig',[
+         //   'productForm' => $productForm->createView()
+        //]);
             //ou 
-            //return $this->renderForm('admin/products/add.html.twiG', compact('productForm'));
-    }
+            return $this->renderForm('admin/products/add.html.twig', compact('productForm'));
+   // }
     
 
-    #[Route('/edition/{id}', name:'edit')]
-    public function edit(Products $products, Request $request, EntityManagerInterface $em): Response 
-    {
+        #[Route('/edition/{id}', name:'edit')]
+        public function edit(Products $products): Response 
+        {
 
         $this->denyAccessUnlessGranted('PRODUCT_EDIT', $products);//verif si utilisateur peut editer avc le voter
 
@@ -76,38 +75,38 @@ class ProductsController extends AbstractController
         // $product->setPrice($prix);
 
         // On crée le formulaire
-        $productForm = $this->createForm(ProductsFormType::class, $product);
+        //$productForm = $this->createForm(ProductsFormType::class, $product);
 
         // On traite la requête du formulaire
-        $productForm->handleRequest($request);
+        //$productForm->handleRequest($request);
 
         //On vérifie si le formulaire est soumis ET valide
-        if($productForm->isSubmitted() && $productForm->isValid()){
+        //if($productForm->isSubmitted() && $productForm->isValid()){
             // On récupère les images
-            $images = $productForm->get('images')->getData();
+           // $images = $productForm->get('images')->getData();
 
                 // On arrondit le prix 
-            $prix = $product->getPrice() * 100;
-            $product->setPrice($prix);
+           // $prix = $product->getPrice() * 100;
+           // $product->setPrice($prix);
 
             // On stocke
-            $em->persist($product);
-            $em->flush();
+           // $em->persist($product);
+           // $em->flush();
 
-            $this->addFlash('success', 'Produit modifié avec succès');
+           // $this->addFlash('success', 'Produit modifié avec succès');
 
             // On redirige
-            return $this->redirectToRoute('admin_products_index');
-        }
+            return $this->render('admin/product/index.html.twig');
+        //}
 
         //return $this->render('admin/products/index.html.twig',[
             //'productForm' => $productForm->createView(),
             //'product' => $product
            // ]};
 
-        return $this->renderForm('admin/products/edit.html.twig', compact('productForm'));
+        //return $this->renderForm('admin/products/edit.html.twig', compact('productForm'));
         // ['productForm' => $productForm]
-    }
+        }
 
 
     #[Route('/supression/{id}', name:'delete')]
